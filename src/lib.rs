a5f02b90e91afd7050d6ab08ffdb1f8363c2534b
Maxwell French <maxwell@frenchnet.org>
2024-01-07 20:26:21 -0500

    Start documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
 pub fn hi() {
     println!("Hello, world!");
 }
+
+
+/*
+The C# version interface includes the following methods:
+
+        int Count { get; }
+
+        string? this[string key] { get; set; }
+
+        string? Get(string key);
+
+        void Add(string key, string? value);
+
+        void Remove(string key);
+
+        bool ContainsKey(string key);
+
+        void Save();
+
+        void SetSecret(string secret);
+
+*/
