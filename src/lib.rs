5fd5edb87f8e803ac3ca5493817dc2fd8763dae7
Maxwell French <maxwell@frenchnet.org>
2024-01-08 21:24:59 -0500

    More documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,22 @@ The C# version interface includes the following methods:
         void SetSecret(string secret);
 
 */
+
+/* the file format is simple
+8,192 bytes long (by default)
+header consisting of the magic bytes MCONF (0x4d, 0x43, 0x4f, 0x4e, 0x46) followed by a reserved byte for versioning
+key length byte, key (UTF-8 byte string) (zero length indicates EOF, the rest is filled with random padding)
+value length byte, value (Null if length is 0)
+
+I.e.:
+
+4d 43 4f 4e 46 vv
+ll xx xx xx xx xx ... mm yy yy yy yy yy ...
+
+first five are magic bytes
+v = version byte
+x = key, y = value
+l = length of key in bytes, m = length of value in bytes
+pattern repeats
+remainder of space is padded with random bytes
+ */
