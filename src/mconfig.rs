9dcb5e1003dd8146ac706a180b370fa3cf56deed
Maxwell French <maxwell@frenchnet.org>
2024-01-14 19:02:46 -0500

    Add get, remove, and contains functions.

--- src/mconfig.rs
+++ src/mconfig.rs
@@ -15,8 +15,8 @@ pub struct MConfig {
 impl MConfig {
     const MAGIC_HEADER_BYTES: [u8; 5] = [0x4d, 0x43, 0x4f, 0x4e, 0x46];
     const MCONFIG_SIZE: usize = 8_192;
-    const MAX_KEY_LEN: usize = 255;
-    const MAX_VALUE_LEN: usize = 255;
+    const MAX_KEY_LEN: usize = u8::MAX as usize;
+    const MAX_VALUE_LEN: usize = u8::MAX as usize;
 
 
     pub fn new() -> MConfig {
@@ -34,10 +34,13 @@ impl MConfig {
 
 
         for (entry_k, entry_v) in self.entries.iter() {
+            assert!(entry_k.len() <= MConfig::MAX_KEY_LEN);
+
             v.push(entry_k.len() as u8);
             v.append(&mut entry_k.as_bytes().to_vec());
 
             if let Some(val) = entry_v {
+                assert!(val.len() <= MConfig::MAX_VALUE_LEN);
                 v.push(val.len() as u8);
                 v.append(&mut val.as_bytes().to_vec());
             }
@@ -45,6 +48,8 @@ impl MConfig {
                 v.push(0);
             }
         }
+        v.push(0);
+        assert!(v.len() <= MConfig::MCONFIG_SIZE);
 
         //pad the rest with random
         for _ in v.len() .. MConfig::MCONFIG_SIZE {
@@ -68,7 +73,31 @@ impl MConfig {
         Ok(self.entries.insert(key, value).unwrap_or(None))
 
     }
+
+    pub fn try_get(&self, key: &String) -> MCResult<&Option<String>> {
+        self.entries.get(key).ok_or(())
+    }
+
+    pub fn get(&self, key: &String) -> Option<&Option<String>> {
+        self.entries.get(key)
+    }
+
+    pub fn contains_key(&self, key: &String) -> bool {
+        self.entries.contains_key(key)
+    }
+
+    pub fn remove(&mut self, key: &String) -> Option<Option<String>> {
+        self.entries.remove(key)
+    }
+
+
 }
 
+impl TryFrom<Vec<u8>> for MConfig {
+    type Error = ();
 
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
 
